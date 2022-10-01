e650af19146f888a8ed6f0638a83629e5c638aa5
Nanak <nanakjotsachdeva@gmail.com>
2022-10-01 10:46:32 +0530

    config

---
+++ config/config.ts
@@ -0,0 +1,265 @@
+export const en = {
+    landing: {
+        title: 'Leading Labor to Jobs Nationwide',
+        subTitle: 'The best online job portal connecting laborers and contractors throughout India.',
+        login: 'log in',
+        getStarted: 'Get started'
+    },
+
+    googleLogIn: {
+        signin: 'Sign in to ExSolution'
+    },
+
+    choosePage: {
+        emp: 'I AM AN EMPLOYER',
+        lab: 'I AM A LABOURER'
+    },
+
+    empRegPage: {
+        title: 'Enter your details',
+        subTitle: 'Employer registration form',
+        nameField: 'Name',
+        cityField: 'City of Residence',
+        phoneField: 'Phone Number',
+        companyField: 'Company',
+        aboutField: 'About Company',
+        button: 'Submit'
+    },
+
+    labRegPage: {
+        title: 'Enter your details',
+        subTitle: 'Labor registration form',
+        nameField: 'Name',
+        cityField: 'City of Residence',
+        phoneField: 'Phone Number',
+        skillsField: 'Skills',
+        langField: 'Preferred Language',
+        eduField: 'Education',
+        button: 'Submit'
+    },
+
+    empDashboard: {
+        empNavChat: 'Chats',
+        empAddJobs: 'Add Jobs',
+        empProfile: 'Profile',
+        empSignout: 'Sign Out',
+        empLabel: 'Employer',
+        title: 'Posted Jobs'
+    },
+
+    labDashboard: {
+        labNavChat: 'Chats',
+        labNot: 'Notifications',
+        labForum: 'Forum',
+        labStarred: 'Starred',
+        labSignout: 'Sign Out',
+        labLabel: 'Worker',
+        labTitle: 'Job Openings'
+    },
+
+    addJob: {
+        title: 'Add Job',
+        subTitle: 'Add details of the job being offered',
+        jobTitle: 'Job Title',
+        jobDesc: 'Job Description',
+        jobPlace: 'Job Place',
+        jobPay: 'Job Pay',
+        workTim: 'Work Timings',
+        skills: 'Skills Required',
+        vacancy: 'Vacancy',
+        submit: 'Submit'
+    },
+
+    empPostedJobs: {
+        empNavChat: 'Chats',
+        empAddJobs: 'Add Jobs',
+        empProfile: 'Profile',
+        empSignout: 'Sign Out',
+        empLabel: 'Employer',
+        title: 'Posted Applications'
+    },
+
+    empLabApplication: {
+        empNavChat: 'Chats',
+        empAddJobs: 'Add Jobs',
+        empProfile: 'Profile',
+        empSignout: 'Sign Out',
+        empLabel: 'Employer',
+        empAccept: 'Accept',
+        empReject: 'Reject'
+    }, 
+
+    popup: {
+        acceptance: 'Employee selected. Now you can chat with the employee in the \'Chats\' section.'
+    },
+
+    labnotifications: {
+        lab_chats: 'Chats',
+        lab_notif: 'Notifications',
+        lab_forum: 'Forum',
+        lab_star: 'Starred',
+        lab_signout: 'Sign Out',
+        lab_label: 'Worker',
+        lab_msg1: 'You are selected with job ID',
+        lab_msg2: 'Congrats! Now you can chat with your employer.'
+    },
+    labforum: {
+        lab_chats: 'Chats',
+        lab_notif: 'Notifications',
+        lab_forum: 'Forum',
+        lab_star: 'Starred',
+        lab_signout: 'Sign Out',
+        lab_label: 'Worker',
+        lab_msg: 'Message'
+    },
+    labapply: {
+        lab_chats: 'Chats',
+        lab_notif: 'Notifications',
+        lab_forum: 'Forum',
+        lab_star: 'Starred',
+        lab_signout: 'Sign Out',
+        lab_label: 'Worker',
+        lab_title: 'Apply',
+        lab_subtitle: 'Job opening',
+        lab_name: 'Name',
+        lab_mno: 'Mobile Number',
+        lab_resid: 'Place of Residence',
+        lab_id: 'ID proof',
+        lab_exp: 'Previous Experience',
+        lab_skills: 'Skills',
+        lab_submit: 'Submit'
+    }
+}
+
+export const hi = {
+    landing: {
+        title: 'देश भर में मजदूरों को रोजगार देना',
+        subTitle: 'पूरे भारत में मजदूरों और ठेकेदारों को जोड़ने वाला सबसे अच्छा समाधान',
+        login: 'प्रवेश करें',
+        getStarted: 'शुरू हो जाओ'
+    },
+
+    googleLogIn: {
+        signin: 'ExSolution में साइन इन करें'
+    },
+
+    choosePage: {
+        emp: 'मैं एक नियोक्ता हूँ',
+        lab: 'मैं एक मजदूर हूँ'
+    },
+
+    empRegPage: {
+        title: 'अपना विवरण दर्ज करें',
+        subTitle: 'नियोक्ता पंजीकरण फॉर्म',
+        nameField: 'नाम',
+        cityField: 'निवास का शहर',
+        phoneField: 'फ़ोन नंबर',
+        companyField: 'संगठन',
+        aboutField: 'संगठन के बारे',
+        button: 'प्रस्तुत करें'
+    },
+
+    labRegPage: {
+        title: 'अपना विवरण दर्ज करें',
+        subTitle: 'नियोक्ता पंजीकरण फॉर्म',
+        nameField: 'नाम',
+        cityField: 'निवास का शहर',
+        phoneField: 'फ़ोन नंबर',
+        skillsField: 'कौशल',
+        langField: 'पसंदीदा भाषा',
+        eduField: 'शिक्षा',
+        button: 'प्रस्तुत करें'
+    },
+
+    empDashboard: {
+        empNavChat: 'चैट',
+        empAddJobs: 'नौकरी लगाओ',
+        empProfile: 'प्रोफ़ाइल',
+        empSignout: 'बाहर जाओ',
+        empLabel: 'नियोक्ता',
+        title: 'पोस्ट की गई नौकरियां',
+    },
+
+    labDashboard: {
+        labNavChat: 'चैट',
+        labNot: 'सूचनाएं',
+        labForum: 'मंच',
+        labStarred: 'महत्वपूर्ण',
+        labSignout: 'बाहर जाओ',
+        labLabel: 'मज़दूर',
+        labTitle: 'नौकरी की रिक्तियां'
+    },
+
+    addJob: {
+        title: 'नौकरी जोड़ें',
+        subTitle: 'ऑफ़र की जा रही नौकरी का विवरण जोड़ें',
+        jobTitle: 'नौकरी का नाम',
+        jobDesc: 'नौकरी का विवरण',
+        jobPlace: 'नौकरी का स्थान',
+        jobPay: 'नौकरी वेतन',
+        workTim: 'काम का समय',
+        skills: 'आवश्यक योग्यता',
+        vacancy: 'रिक्ति',
+        submit: 'प्रस्तुत करें'
+    },
+
+    empPostedJobs: {
+        empNavChat: 'चैट',
+        empAddJobs: 'नौकरी लगाओ',
+        empProfile: 'प्रोफ़ाइल',
+        empSignout: 'बाहर जाओ',
+        empLabel: 'नियोक्ता',
+        title: 'पोस्ट किए गए आवेदन',
+    },
+
+    empLabApplication: {
+        empNavChat: 'चैट',
+        empAddJobs: 'नौकरी लगाओ',
+        empProfile: 'प्रोफ़ाइल',
+        empSignout: 'बाहर जाओ',
+        empLabel: 'नियोक्ता',
+        empAccept: 'स्वीकार',
+        empReject: 'अस्वीकार'
+    },
+
+    popup: {
+        acceptance: 'कर्मचारी चयनित। अब आप \'चैट\' सेक्शन में कर्मचारी से बात कर सकते हैं।'
+    },
+
+    labnotifications: {
+        lab_chats: 'चैट',
+        lab_notif: 'सूचनाएं',
+        lab_forum: 'मंच',
+        lab_star: 'महत्वपूर्ण',
+        lab_signout: 'साइन आउट',
+        lab_label: 'मज़दूर',
+        lab_msg1: 'आपको यह जॉब आईडी के साथ चुना गया है',
+        lab_msg2: 'बधाई! अब आप अपने नियोक्ता के साथ चैट कर सकते हैं।'
+    },
+    labforum: {
+        lab_chats: 'चैट',
+        lab_notif: 'सूचनाएं',
+        lab_forum: 'मंच',
+        lab_star: 'महत्वपूर्ण',
+        lab_signout: 'साइन आउट',
+        lab_label: 'मज़दूर',
+        lab_msg: 'संदेश'
+    },
+    labapply: {
+        lab_chats: 'चैट',
+        lab_notif: 'सूचनाएं',
+        lab_forum: 'मंच',
+        lab_star: 'महत्वपूर्ण',
+        lab_signout: 'साइन आउट',
+        lab_label: 'मज़दूर',
+        lab_title: 'लागू',
+        lab_subtitle: 'नौकरी की उद्घाटन',
+        lab_name: 'नाम',
+        lab_mno: 'मोबाइल नंबर',
+        lab_resid: 'निवास की जगह',
+        lab_id: 'आईडी प्रूफ',
+        lab_exp: 'पूर्व अनुभव',
+        lab_skills: 'कौशल',
+        lab_submit: 'प्रस्तुत करना'
+    }
+}
