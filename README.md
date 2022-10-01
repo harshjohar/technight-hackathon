53e925db2ba4f6e306e3503827c39bcfc7b0db83
harsh johar <hpsj3112@gmail.com>
2022-10-01 14:37:55 +0530

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Technight hackthon coducted by IEEE
+
+Website demo : https://technight-hackathon.vercel.app
+
+Video demo: https://www.youtube.com/watch?v=0I4dxXI1jp0
+
+PPT: https://docs.google.com/presentation/d/1Kff9RiMg6jGpD1sI-BJbjA92h8ybKceyjhahEV8fcvE/edit?usp=sharing
+
+This is an interface built to solve the problem of unemployment of skilled and unskilled labourers upto some extent.
+
+Introducing ExSolution : The best online job portal connecting labourers and contractors throughout India.
+
+The problem to solve : The amount of unemployed people in India despite there still being vacant job openings means theres a gap between contractors and labourers. There was need of a user-friendly software that could easily be understood by both educated and uneducated people. 
+
+Our solution : A linkedIn for workers with intuitive design and multilingual support to allow both workers to easily search and apply for jobs and for the employers to add jobs and manage applicants all within our application.
--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import React from "react";
+import Nav from "../../components/common/NavbarLabor";
 
 function index() {
     return (
@@ -8,7 +9,19 @@ function index() {
                 <title>Chat</title>
                 <link rel="icon" href="/assets/favicon/favicon.ico" />
             </Head>
-            <a href="https://fabchat.vercel.app/">Chat here for now</a>
+            <Nav />
+            <div className="w-screen h-screen flex flex-col justify-around items-center">
+                <div className="flex flex-col items-center">
+                    <a href="https://fabchat.vercel.app/" className="text-xl">
+                        Chat <span className="text-blue-300">here</span> for now
+                    </a>
+                    <p>
+                        This is another platform built by the same organisation
+                        in one of their previous projects. In future, it is
+                        aimed to embed this chat inside this platform only.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
